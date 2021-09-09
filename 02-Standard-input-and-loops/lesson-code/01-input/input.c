be44801bb2884b4d31991bc3d23b68a20b28c30d
Ginmaru-Gin <ramin.ginmaru@gmail.com>
2021-09-09 20:52:58 +0500

    fix: adds multiple input example in Aud2

--- 02-Standard-input-and-loops/lesson-code/01-input/input.c
+++ 02-Standard-input-and-loops/lesson-code/01-input/input.c
@@ -9,11 +9,12 @@ int main(void) {
     // который мы можем получить используя унарный оператор &
     int age;
     float pi;
+    double e;
     printf_s("Enter your age: ");
     scanf_s("%d", &age); // iVar -- переменная, а &iVar -- адрес этой переменной
-    printf_s("Enter the Pi value: ");
-    scanf_s("%f", &pi);
-    printf_s("So, your age is %d and pi value is %f!\n", age, pi);
+    printf_s("Enter Pi and Exp value: ");
+    scanf_s("%f%lf", &pi, &e);
+    printf_s("So, your age is %d and pi value is %f and e = %lf!\n", age, pi, e);
 
     system("pause");
     return 0;
