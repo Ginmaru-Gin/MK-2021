bae0e0eb8d71a656e763d867deb20a67f7a13064
Ginmaru-Gin <ramin.ginmaru@gmail.com>
2021-09-29 07:05:21 +0500

    adds Aud5
    feat: adds Aud5
    fix: minor typo correction

---
+++ 05-Functions-pointers-files/task/template/task-1/task-1.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+#define MAX_ARRAY_SIZE 100
+#define INPUT_FILE_NAME "numbers"
+#define OUTPUT_FILE_NAME "sorted"
+
+void sort(int[MAX_ARRAY_SIZE]);
+
+int main(void) {
+    FILE* input, *output;
+    int arr[MAX_ARRAY_SIZE];
+    // ...
+    sort(arr);
+    // ...
+
+    return 0;
+}
+
+void sort(int arr[MAX_ARRAY_SIZE]) {
+    // ...
+}
