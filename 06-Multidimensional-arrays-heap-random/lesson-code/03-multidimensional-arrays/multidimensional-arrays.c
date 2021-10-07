7997b44794bcf376a2abd1724dd33cefc700fb4b
Ginmaru-Gin <ramin.ginmaru@gmail.com>
2021-10-07 14:03:03 +0500

    changes lesson-code and task for Aud6

--- 06-Multidimensional-arrays-heap-random/lesson-code/03-multidimensional-arrays/multidimensional-arrays.c
+++ 06-Multidimensional-arrays-heap-random/lesson-code/03-multidimensional-arrays/multidimensional-arrays.c
@@ -2,13 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ERR_ALLOC_MEM "ERROR: couldn't allocate memory\nEXIT WITH ERROR CODE 1\n"
-
 #define ARRAY_WIDTH 10
 #define ARRAY_HEIGHT 5
 
 void printStaticArray(int [ARRAY_HEIGHT][ARRAY_WIDTH]);
-void printArray(int**, int height, int width);
 
 int main(void) {
     srand((unsigned)time(NULL));
@@ -34,100 +31,6 @@ int main(void) {
         printf_s("\n");
     }
 
-    // ДИНАМИЧЕСКИЕ МАССИВЫ
-    
-    // динамический двумерный массив представляет собой массив указателей:
-    int height, width;
-    printf_s("Enter size (HxW): ");
-    scanf_s("%dx%d", &height, &width);
-    int** dynamicArr = malloc(height * sizeof(int*));
-    if (!dynamicArr) {
-        printf_s(ERR_ALLOC_MEM);
-        return 1;
-    }
-    // теперь нужно выделить память для каждого подмассива
-    for (int i = 0; i < height; ++i) {
-        // в случае неудачи нужно освободить память,
-        // которую мы уже выделили
-        if (!(dynamicArr[i] = malloc(width * sizeof(int)))) {
-            for (int j = 0; j < i; ++j) {
-                free(dynamicArr[j]);
-            }
-            free(dynamicArr);
-            printf_s(ERR_ALLOC_MEM);
-            return 1;
-        }
-    }
-    // заполним этот массив таблицей умножения
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            dynamicArr[i][j] = (i + 1) * (j + 1);
-        }
-    }
-    printArray(dynamicArr, height, width);
-
-    // освобождать память дин. массива нужно изнутри
-    for (int i = 0; i < height; ++i) {
-        free(dynamicArr[i]);
-    }
-    free(dynamicArr);
-
-    // создадим двумерный массив комплексных чисел, которые будут
-    // организованы как массивы из двух вещественных чисел
-    // т.е. это будет трехмерный массив
-    double*** complexArr = malloc(height * sizeof(double**));
-    if (!complexArr) {
-        printf_s(ERR_ALLOC_MEM);
-        return 1;
-    }
-    for (int i = 0; i < height; ++i) {
-        if (!(complexArr[i] = malloc(width * sizeof(double*)))) {
-            for (int j = 0; j < i; ++j) {
-                free(complexArr);
-            }
-            free(complexArr);
-            printf_s(ERR_ALLOC_MEM);
-            return 1;
-        }
-    }
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            if (!(complexArr[i][j] = malloc(2 * sizeof(double)))) {
-                // освобождаем предыдущие строки
-                for (int n = 0; n < i; ++n) {
-                    for (int m = 0; m < width; ++m) {
-                        free(complexArr[n][m]);
-                    }
-                    free(complexArr[n]);
-                }
-                // освобождаем текущую строку
-                for (int n = 0; n < j; ++n) {
-                    free(complexArr[i][n]);
-                }
-                free(complexArr[i]);
-                // освобождаем внешний массив
-                free(complexArr);
-                printf_s(ERR_ALLOC_MEM);
-                return 1;
-            }
-        }
-    }
-    // память аллоцирована, теперь нагенерируем числа
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            for (int k = 0; k < 2; ++k) {
-                complexArr[i][j][k] = (double)rand() / RAND_MAX * 100;
-            }
-        }
-    }
-    // выведем
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            printf_s("(%.2lf, %.2lf)\t", complexArr[i][j][0], complexArr[i][j][1]);
-        }
-        printf_s("\n");
-    }
-
     system("pause");
     return 0;
 }
@@ -135,15 +38,6 @@ int main(void) {
 void printStaticArray(int arr[ARRAY_HEIGHT][ARRAY_WIDTH]) {
     for (int i = 0; i < ARRAY_HEIGHT; ++i) {
         for (int j = 0; j < ARRAY_WIDTH; ++j) {
-            printf_s("%d\t", arr[i][j]);
-        }
-        printf_s("\n");
-    }
-}
-
-void printArray(int** arr, int height, int width) {
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
             printf_s("%d\t", arr[i][j]);
         }
         printf_s("\n");
